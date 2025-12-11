21fd6222f02c3fb8a35d735426e508045ddd462c
Marzz <rjackman1983@me.com>
2025-12-10 23:16:35 -0600

    created binary trees file

--- binary_trees.lua
+++ binary_trees.lua
@@ -2,3 +2,13 @@
 -- They cannot have more than two children.
 
 -- Common uses of a binary trees are binary search trees, binary heaps and Huffman coding.
+
+-- Define a node structure for the binary tree_damage
+
+local Node = {}
+
+Node.__index = Node
+
+function Node.new(value)
+
+end
