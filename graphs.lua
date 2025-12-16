66626cc3938e7f7888516478c34cb764be8c459a
Marzz <rjackman1983@me.com>
2025-12-15 21:15:32 -0600

    Started Graphs.

---
+++ graphs.lua
@@ -0,0 +1,6 @@
+-- A graph G is a pair (V,A) with V being a finite nin-empty set and A a set of pairs that represent the edges of the graph.
+    -- The elements of V are called vertices(or nodes).
+    -- The elements of A are called edges.
+    -- the number of vertices |V| is called order of G.
+    -- The number of edges |A| is called size of G.
+
